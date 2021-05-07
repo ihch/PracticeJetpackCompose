85c7a1ccd6b7bb1df2f978ef105f8a5554c30088
ihch <nemu0sou@gmail.com>
2021-05-07 15:18:29 +0900

    feat: added modifier padding

--- app/src/main/java/com/example/practicejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/practicejetpackcompose/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
         Text("A day in Shark Fin Cove")
         Text("O-tani san!")
         Text("10 home run!")
