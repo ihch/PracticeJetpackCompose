32c03953050afe5c69186eae6f5798a6b0e8dfa4
ihch <nemu0sou@gmail.com>
2021-05-07 16:12:42 +0900

    feat: apply material theme

--- app/src/main/java/com/example/practicejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/practicejetpackcompose/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -25,22 +29,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.header),
-            contentDescription = null,
-            modifier = Modifier
-                .paddingFromBaseline(0.dp, bottom = 12.dp)
-                .height(180.dp)
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop,
-        )
-        Spacer(modifier = Modifier.padding(8.dp))
-        Text("A day in Shark Fin Cove")
-        Text("O-tani san!")
-        Text("10 home run!")
+    MaterialTheme() {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .paddingFromBaseline(0.dp, bottom = 12.dp)
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop,
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                "MLB Los Angels " +
+                        "Big fly O-tani san! " +
+                        "next day of pitching.",
+                style = typography.h6,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Text("O-tani san!", style = typography.body2)
+            Text("10 home run!", style = typography.body2)
+        }
     }
 }
 
